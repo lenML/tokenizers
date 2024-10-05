0f298b0519f4fb773d11dc568565565273916420
zhzluke96 <zhzluke96@outlook.com>
2024-10-05 15:25:09 +0800

    :sparkles: publish to @3

--- packages/gpt4/tsup.config.ts
+++ packages/gpt4/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig((options) => {
   return {
     entry: ["src/main.ts"],
     format: ["cjs", "esm", "iife"],
+    target: "es2020",
     dts: true,
     splitting: false,
     sourcemap: true,
