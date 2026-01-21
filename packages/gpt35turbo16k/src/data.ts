7d68d3b3ed8d9b5f554c02ef7ddb4a8b7d60676a
zhzluke96 <zhzluke96@outlook.com>
2026-01-22 00:48:26 +0800

    feat: remove auto generated

--- packages/gpt35turbo16k/src/data.ts
+++
@@ -1,9 +0,0 @@
-import type { NSTokenizerConfig, NSTokenizerJSON } from "@lenml/tokenizers";
-
-import * as rawTokenizerJSON from "../models/tokenizer.json";
-import * as rawTokenizerConfig from "../models/tokenizer_config.json";
-
-const tokenizerJSON = rawTokenizerJSON as unknown as NSTokenizerJSON.Root;
-const tokenizerConfig = rawTokenizerConfig as unknown as NSTokenizerConfig.Root;
-
-export { tokenizerJSON, tokenizerConfig };
---
+++ pretrained/readme.md
@@ -0,0 +1,5 @@
+# pretrained
+
+This folder will contain automatically generated code for packaging and deployment.
+
+此文件夹下将创建自动生成的代码，用于打包和发布
